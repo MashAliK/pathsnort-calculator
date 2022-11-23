fd28503902e8d3e3acd97af97447fc1d336c2da3
MashAliK <mashhoodkhan71@gmail.com>
2022-11-22 22:39:14 -0500

    Created PathSnort Calculator

---
+++ src/Chain.java
@@ -0,0 +1,19 @@
+public class Chain {
+    private static final int L = 0;
+    private static final int R = 1;
+    private static final int U = 2;
+    private int left;
+    private int right;
+    private int len;
+
+    public Chain(int left, int right, int len){
+        this.left = left;
+        this.right = right;
+        this.len = len;
+    }
+
+    public boolean move(int i){
+
+        return false;
+    }
+}
